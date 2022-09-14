78c399663a97140850efdfe0e3b03159a4d2e8a0
palla023 <65664262+palla023@users.noreply.github.com>
2022-09-14 19:43:46 +0530

    Update custom.js

--- custom.js
+++ custom.js
@@ -33,7 +33,7 @@ function checkBottom(colIndex) {
 }
 
 function colorMatchCheck(one,two,three,four){
-  return (one===two && one===three && one===four && one !== 'rgb(128, 128, 128)' && one !== undefined);
+   return (one===two && one===three && one===four && one !== 'rgb(128, 128, 128)' && one !== undefined);
 }
 
 function horizontalWinCheck() {
